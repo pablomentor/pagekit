c45440ad203149ea880d04ae320a3ed142cf4097
pablomentor <60623348+pablomentor@users.noreply.github.com>
2020-02-28 02:31:43 +0100

    Update login.php
    The icons are already printed with <?= $view->render('head') ?>

--- app/system/modules/theme/views/login.php
+++ app/system/modules/theme/views/login.php
@@ -4,8 +4,6 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="<?= $view->url()->getStatic('system/theme:favicon.ico') ?>" rel="shortcut icon" type="image/x-icon">
-        <link href="<?= $view->url()->getStatic('system/theme:apple_touch_icon.png') ?>" rel="apple-touch-icon-precomposed">
         <?= $view->render('head') ?>
         <?php $view->style('theme', 'system/theme:css/theme.css') ?>
         <?php $view->script('login', 'system/theme:js/login.js', ['uikit']) ?>
